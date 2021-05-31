800088a41e07cf42aa171f6fa7e53861c998110e
soyolim-txter <healingdaybut@gmail.com>
2021-05-31 19:14:39 +0900

    R3 Django fin alphatest before making CRUD

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 # Create your views here.
+from accountapp.models import TextDB
+
 
 def home(request):
 
     if request.method == 'POST':
         temp = request.POST.get('input_text')
-        return render(request,'accountapp/home.html',context={'text':temp})
+        initial_DB = TextDB()
+        initial_DB.text = temp
+        initial_DB.save()
+
+        return HttpResponseRedirect('.')
     else:
-        return render(request, 'accountapp/home.html', context={'text':'.'})
+        text = TextDB.objects.all()
+
+        return render(request, 'accountapp/home.html', context={'text':text})
