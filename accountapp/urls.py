5b5d067361b4d4a5156d92f14114322ff74c7600
soyolim-txter <healingdaybut@gmail.com>
2021-06-01 21:39:30 +0900

    R3 django CRUD initial challenge

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include, reverse_lazy
 
-from accountapp.views import home, AccountCreateView
+from accountapp.views import home, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path('',home,name='home'),
     path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
-    path('create/',AccountCreateView.as_view(),name='create')
+    path('create/',AccountCreateView.as_view(),name='create'),
+    path('mypage/<int:pk>',AccountDetailView.as_view(),name='detail'),
+    path('change/<int:pk>',AccountUpdateView.as_view(),name='update'),
+    path('quit/<int:pk>',AccountDeleteView.as_view(),name='delete'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.forms import UserUpdateForm
 from accountapp.models import TextDB
 
 
@@ -29,3 +30,21 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:home')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name =  'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = 'target_user'
+    form_class = UserUpdateForm
+    success_url = reverse_lazy('accountapp:home')
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:home')
+    template_name =  'accountapp/delete.html'
